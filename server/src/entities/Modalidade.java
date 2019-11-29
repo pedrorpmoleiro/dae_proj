e13d291e5e8ce9c749cd18e7875411b01ceca7a1
Andres <andrestoledo@outlook.pt>
2019-11-29 11:59:15 +0000

    testado a correr a base dados

--- server/src/entities/Modalidade.java
+++ server/src/entities/Modalidade.java
@@ -15,29 +15,23 @@ import java.util.Set;
 @Table(name="MODALIDADES")
 public class Modalidade implements Serializable {
    // private List<Socio> socios;
+   /*
    @ManyToMany(mappedBy = "modalidades")
     private Set<Treinador> treinadores;
+    */
    @Id
     private String nome;
    // private List<Escalao> escaloes;
 
     public Modalidade( String nome) {
       //  this.socios = new LinkedList<Socio>();
-        this.treinadores = new LinkedHashSet<>();
+     //   this.treinadores = new LinkedHashSet<>();
         this.nome = nome;
       //  this.escaloes = new LinkedList<Escalao>();
     }
     public Modalidade() {
         this("");
     }
-    public Set<Treinador> getTreinadores(){
-        return this.treinadores;
-    }
-
-    public void setTreinadores(Set<Treinador> treinadores) {
-        this.treinadores = treinadores;
-    }
-
     public String getNome() {
         return nome;
     }
