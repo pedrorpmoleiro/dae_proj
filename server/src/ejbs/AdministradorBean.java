5f215ddf1fed4b96f138237e0ede10e54e6e859a
Kevin Merizalde Oñate <kevinmerizalde19@outlook.com>
2019-11-29 10:16:47 +0000

    Adicione a herança entre socio => treinador , atleta / user => socio , Administrador // Crie Socio (entity)  /// Adicione Beans para os tipos de utilizadores

---
+++ server/src/ejbs/AdministradorBean.java
@@ -0,0 +1,9 @@
+package ejbs;
+
+import javax.ejb.Stateless;
+
+@Stateless(name = "AdministradorEJB")
+public class AdministradorBean {
+    public AdministradorBean() {
+    }
+}
--- server/src/entities/Atleta.java
+++ server/src/entities/Atleta.java
@@ -6,6 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 /*
@@ -15,10 +16,10 @@ import java.io.Serializable;
                 query = "SELECT s FROM Student s ORDER BY s.name" // JPQL
         )
 })*/
-public class Atleta  implements Serializable {
+public class Atleta extends Socio implements Serializable {
         @Id
         private int id;
-
+        //private List<Atleta> atletas;
         public Atleta() {
         }
 }
