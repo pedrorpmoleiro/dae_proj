8d9e4cbdfbdd964f87e9bfe0eb69e8c3e144eb27
Andres <andrestoledo@outlook.pt>
2019-12-26 16:51:08 +0000

    cambios

--- client/pages/modalidades/index.vue
+++ client/pages/modalidades/index.vue
@@ -6,15 +6,15 @@
                 <v-text-field
                         v-model="search"
                         append-icon="search"
-                        label="Search"
+                        label="Procurar"
                         single-line
                         hide-details
                 ></v-text-field>
             </v-card-title>
-            <v-data-table :headers="headers" :items="items" :items-per-page="5" :search="search" class="elevation-1">
+            <v-data-table :headers="headers" :items="items" :items-per-page="5" :search="search" class="elevation-1"   show-expand>
                 <template v-slot:top>
                     <v-toolbar flat color="dark">
-                        <v-toolbar-title>My CRUD</v-toolbar-title>
+                        <v-toolbar-title>Informação</v-toolbar-title>
                         <v-divider class="mx-4" inset vertical></v-divider>
                         <v-spacer></v-spacer>
                     </v-toolbar>
@@ -46,9 +46,16 @@
                         </v-card>
                     </v-dialog>
                 </template>
+                <template v-slot:expanded-item="{headers }">
+                    <v-card class="mx-auto" max-width="344">
+                        <v-card-text>
+                            <div>Escaloes Information</div>
+                        </v-card-text>
+                    </v-card>
+                </template>
                 <template v-slot:item.action="{ item }">
                     <v-icon small class="mr-2" @click="">edit</v-icon>
-                    <v-icon small @click=""> delete</v-icon>
+                    <v-icon small @click="deleteItem(item)"> delete</v-icon>
                 </template>
             </v-data-table>
         </v-card>
@@ -62,8 +69,8 @@
             return {
                 dialog: false,
                 items: [],
-                headers: [{text: 'Name', align: 'left', sortable: false, value: 'nome'}, {
-                    text: 'Actions',
+                headers: [{text: 'Nome', align: 'left', sortable: false, value: 'nome'}, { text: '', value: 'data-table-expand' },{
+                    text: 'Acciones',
                     value: 'action',
                     sortable: false
                 }],
@@ -74,10 +81,7 @@
             }
         },
         created() {
-            this.$axios.get("/api/modalidades")
-                .then(r => {
-                    this.items = r.data;
-                })
+           this.getModalidades();
         },
         methods: {
             close() {
@@ -93,12 +97,25 @@
                     headers: {'Content-Type': 'application/json'}
                 })
                     .then(e => {
-                            this.$axios.get("/api/modalidades")
-                                .then(r => {
-                                    this.items = r.data;
-                                })
+                           this.getModalidades();
                         }
                     )
+            },
+            deleteItem(item){
+               item.nome
+                confirm('Are you sure you want to delete this item?')&&
+                this.$axios({method:'delete',url:'/api/modalidades/'+item.nome})
+                    .then(e=>{
+                        console.log('Delete')
+                        this.getModalidades();
+
+                    })
+            },
+            getModalidades(){
+                this.$axios.get("/api/modalidades")
+                    .then(r => {
+                        this.items = r.data;
+                    })
             }
 
         }
