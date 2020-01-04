2cc25d34b3df9a098e23c89f688dfdec8bd8ff20
Pedro Moleiro <pedro.r.p.moleiro@gmail.com>
2020-01-04 20:42:55 -0100

    Auto stash before merge of "feature/US9" and "origin/feature/US2"

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,35 +1,81 @@
 <template>
-  <v-app>
-    <div>
-      <nuxt />
-    </div>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 2020</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        clipped: false,
-        drawer: false,
-        fixed: false,
-        items: [
-          {
-            icon: 'mdi-apps',
-            title: 'Welcome',
-            to: '/'
-          },
-          {
-            icon: 'mdi-chart-bubble',
-            title: 'Inspire',
-            to: '/inspire'
-          }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-      }
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          title: 'Home',
+          to: '/'
+        },
+        {
+          title: 'Pagamentos',
+          to: '/pagamentos'
+        },
+        {
+          title: 'Produtos',
+          to: '/produtos'
+        },
+        {
+          title: 'Utilizadores',
+          to: '/utilizadores'
+        },
+        {
+          title: 'Modalidades',
+          to: '/modalidades'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Gestão de Clubes Desportivos'
     }
   }
 </script>
