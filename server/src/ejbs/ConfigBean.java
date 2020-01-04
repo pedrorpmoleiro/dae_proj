3a70ac76e677f6b33e167345adbf75a982f4ebc1
Pedro Moleiro <pedro.r.p.moleiro@gmail.com>
2020-01-04 22:48:50 -0100

    WEEEE

--- server/src/ejbs/ConfigBean.java
+++ server/src/ejbs/ConfigBean.java
@@ -33,44 +33,44 @@ public class ConfigBean {
     private ProductBean productBean;
     @EJB
     private PaymentBean paymentBean;
-    
     @EJB
     private HorarioBean horarioBean;
+
     public ConfigBean() {
     }
 
     @PostConstruct
     public void populateDB() {
         try {
-            //Epoca
             epocaBean.create("2018-2019");
             epocaBean.create("2019-2020");
 
-            // PASSWORD
+            modalidadeBean.create("BASQUETBALL","2018-2019");
+            modalidadeBean.create("BASQUETBALL","2019-2020");
+            modalidadeBean.create("SOCCER","2019-2020");
+
+            escalaoBean.create("SUB18","SOCCER","2019-2020");
+            escalaoBean.create("SUB20","BASQUETBALL","2018-2019");
+
             String password = "123123";
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
             String passwordHash = Base64.getEncoder().encodeToString(hash);
 
-            // Admins
             administradorBean.create("kenalx1999", passwordHash, "kevin", "kevin@gmail.com");
             administradorBean.create("chaca", passwordHash, "andres", "andres@gmail.com");
             administradorBean.create("pedrinsky", passwordHash, "pedro", "pedro@gmail.com");
 
-            // Treinador
             treinadorBean.create("lucho", passwordHash, "luis", "luis@gmail.com");
             treinadorBean.create("mateo", passwordHash, "mateo", "mateo@gmail.com");
 
-            // Atleta
             atletaBean.create("mario", passwordHash, "mario", "mario@gmail.com");
             atletaBean.create("santiago", passwordHash, "santiago", "santiago@gmail.com");
 
-            // PRODUCTS
             productBean.create(1, ProductType.AULA, "123", 24.0);
             productBean.create(2, ProductType.ARTIGO, "123", 124.0);
             productBean.create(3, ProductType.INSCRICAO, "123", 424.0);
 
-            // PAYMENTS
             paymentBean.create(1, "lucho", 1, Instant.now().getEpochSecond(), 2, 200.0,
                     PaymentStatus.PAID, "RECEIPT");
             paymentBean.create(2, "mario", 3, Instant.now().getEpochSecond(), 1, 50.0,
@@ -78,23 +78,13 @@ public class ConfigBean {
             paymentBean.create(3, "mateo", 2, Instant.now().getEpochSecond(), 6, 2400.0,
                     PaymentStatus.PARCIAL, "RECEIPT");
 
-            // MODALIDADE
-            modalidadeBean.create("BASQUETBALL","2018-2019");
-            modalidadeBean.create("BASQUETBALL","2019-2020");
-            modalidadeBean.create("SOCCER","2019-2020");
-
             modalidadeBean.enrollAtletaEscalao("SUB20","mario","2018-2019","BASQUETBALL");
             modalidadeBean.enrollAtletaEscalao("SUB18","santiago","2019-2020","SOCCER");
             modalidadeBean.enrollTreinadorEscalao("SUB20","lucho","2018-2019","BASQUETBALL");
 
-            // ESCALAO
-            escalaoBean.create("SUB18","SOCCER","2019-2020");
-            escalaoBean.create("SUB20","BASQUETBALL","2018-2019");
-
             escalaoBean.createAula("2018-2019","BASQUETBALL","SUB20",TipoDia.valueOf("SEGUNDA_FEIRA"),"pilates",10,12);
             escalaoBean.createAula("2018-2019","BASQUETBALL","SUB20",TipoDia.valueOf("TERCA_FEIRA"),"trote",10,12);
 
-            // HORARIO
             horarioBean.createAssiduidade("lucho","SUB20","2018-2019",TipoDia.valueOf("SEGUNDA_FEIRA"),10,12,"mario",true);
 
         } catch (Exception e) {
